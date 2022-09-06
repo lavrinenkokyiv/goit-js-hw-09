978d9f7f4762d00655c4af38a400d1d8bf5f97a7
lavrinenkokyiv <88554016+lavrinenkokyiv@users.noreply.github.com>
2022-09-06 13:24:01 +0300

    fully loaded

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+const backgroundColorSwitcher = function () {
+  body.style.backgroundColor = getRandomHexColor();
+  body.style.color = getRandomHexColor();
+};
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(backgroundColorSwitcher, 1000);
+  btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
